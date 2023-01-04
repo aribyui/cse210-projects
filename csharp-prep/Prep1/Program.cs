93ee27e7936fac842155318ba340df3066de1893
aribyui <ari22008@byui.edu>
2023-01-04 14:30:17 -0600

    Finished Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("\nWhat is your first name? ");
+        string name = Console.ReadLine();
+        name = char.ToUpper(name[0]) + name.Substring(1); // https://www.delftstack.com/howto/csharp/csharp-capitalize-first-letter/
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        last = char.ToUpper(last[0]) + last.Substring(1); // https://www.delftstack.com/howto/csharp/csharp-capitalize-first-letter/
+
+        Console.WriteLine($"\nYour name is: {last}, {name} {last}.\n");
+    
     }
 }
