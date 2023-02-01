2081a17c272ca88315e8700dcae8089a6a2e4647
aribyui <ari22008@byui.edu>
2023-01-31 19:57:06 -0600

    Finished Learning03

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,60 @@
+public class Fraction
+{
+    // attributes
+    public int _top;
+    public int _bottom;
+
+    // constructors
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    // methods
+    public string GetFractionString()
+    {
+        // return $"{_top}/{_bottom}";
+        
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+}
